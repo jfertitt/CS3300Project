5dba5fa10b42496f39a8bd1649295f51ac81d5b8
Joseph Fertitta <jfertitt@uccs.edu>
2021-04-28 02:59:27 +0000

    Init

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
 
-  config.action_mailer.default_url_options = { process.env.PORT, port: 3000 }
+  config.action_mailer.default_url_options = { process.env.PORT, port: 5000}
 
   config.action_mailer.raise_delivery_errors = false
 
